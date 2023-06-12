47e7bc7a7020c49ab3c40dd0b9777ad79b274e45
nikogamsahurdia <eleron96@gmail.com>
2023-06-12 18:47:49 +0300

    updated app and db

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for, flash
-from .db import get_connection
+from .db import get_conn, release_conn, init_db_pool
 from .urls import validate, normilize
 import requests
 from bs4 import BeautifulSoup
@@ -10,11 +10,11 @@ load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 
+init_db_pool()  # Инициализация пула соединений с базой данных
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 @app.post("/urls")
 def add_url():
@@ -26,54 +26,62 @@ def add_url():
             return render_template("index.html", url=url), 422
 
     normalized_url = normilize(url)  # нормализация URL
-    conn = get_connection()
+    conn = get_conn()
     cursor = conn.cursor()
-    cursor.execute("""
-    SELECT id FROM urls WHERE name = %s
-    """, (normalized_url,))  # используем нормализованный URL
-    existing_url = cursor.fetchone()
-
-    if existing_url is None:
+    try:
         cursor.execute("""
-        INSERT INTO urls(name)
-        VALUES (%s)
-        RETURNING id
+        SELECT id FROM urls WHERE name = %s
         """, (normalized_url,))  # используем нормализованный URL
-        url_id = cursor.fetchone()[0]
-        flash("Страница успешно добавлена", "success")
-    else:
-        url_id = existing_url[0]
-        flash("Страница уже существует", "warning")
+        existing_url = cursor.fetchone()
 
-    conn.commit()
+        if existing_url is None:
+            cursor.execute("""
+            INSERT INTO urls(name)
+            VALUES (%s)
+            RETURNING id
+            """, (normalized_url,))  # используем нормализованный URL
+            url_id = cursor.fetchone()[0]
+            flash("Страница успешно добавлена", "success")
+        else:
+            url_id = existing_url[0]
+            flash("Страница уже существует", "warning")
+
+        conn.commit()
+    finally:
+        release_conn(conn)
+
     return redirect(url_for('show_url', url_id=url_id))
+
 
 
 @app.route("/urls/<int:url_id>")
 def show_url(url_id):
-    conn = get_connection()
+    conn = get_conn()
     cursor = conn.cursor()
-    cursor.execute("SELECT id, name, created_at FROM urls WHERE id = %s",
-                   (url_id,))
-    url = cursor.fetchone()
+    try:
+        cursor.execute("SELECT id, name, created_at FROM urls WHERE id = %s",
+                       (url_id,))
+        url = cursor.fetchone()
 
-    cursor.execute(
-        "SELECT id, url_id, created_at, status_code, h1, description, "
-        "title FROM url_checks WHERE url_id = %s ORDER BY created_at DESC",
-        (url_id,))
-    checks_raw = cursor.fetchall()
+        cursor.execute(
+            "SELECT id, url_id, created_at, status_code, h1, description, "
+            "title FROM url_checks WHERE url_id = %s ORDER BY created_at DESC",
+            (url_id,))
+        checks_raw = cursor.fetchall()
 
-    checks = []
-    for check in checks_raw:
-        checks.append({
-            "id": check[0],
-            "url_id": check[1],
-            "created_at": check[2],
-            "status_code": check[3],
-            "h1": check[4],
-            "description": check[5],
-            "title": check[6]
-        })
+        checks = []
+        for check in checks_raw:
+            checks.append({
+                "id": check[0],
+                "url_id": check[1],
+                "created_at": check[2],
+                "status_code": check[3],
+                "h1": check[4],
+                "description": check[5],
+                "title": check[6]
+            })
+    finally:
+        release_conn(conn)
 
     return render_template("url.html", id=url[0], name=url[1],
                            created_at=url[2], checks=checks)
@@ -81,65 +89,73 @@ def show_url(url_id):
 
 @app.route("/urls")
 def urls_list():
-    conn = get_connection()
+    conn = get_conn()
     cursor = conn.cursor()
-    cursor.execute("""
-    SELECT urls.id, urls.name, checks.created_at, checks.status_code
-    FROM urls
-    LEFT JOIN (
-        SELECT url_id, status_code, created_at,
-            ROW_NUMBER() OVER (
-                PARTITION BY url_id ORDER BY created_at DESC) as rn
-        FROM url_checks
-    ) checks ON urls.id = checks.url_id
-    WHERE checks.rn = 1
-    ORDER BY CASE WHEN checks.created_at IS NULL THEN 1 ELSE 0 END,
-        checks.created_at DESC
-    """)
-    urls_raw = cursor.fetchall()
+    try:
+        cursor.execute("""
+        SELECT urls.id, urls.name, checks.created_at, checks.status_code
+        FROM urls
+        LEFT JOIN (
+            SELECT url_id, status_code, created_at,
+                ROW_NUMBER() OVER (
+                    PARTITION BY url_id ORDER BY created_at DESC) as rn
+            FROM url_checks
+        ) checks ON urls.id = checks.url_id
+        WHERE checks.rn = 1
+        ORDER BY CASE WHEN checks.created_at IS NULL THEN 1 ELSE 0 END,
+            checks.created_at DESC
+        """)
+        urls_raw = cursor.fetchall()
 
-    urls = []
-    for url in urls_raw:
-        urls.append({
-            "id": url[0],
-            "name": url[1],
-            "created_at": url[2],
-            "status_code": url[3]
-        })
+        urls = []
+        for url in urls_raw:
+            urls.append({
+                "id": url[0],
+                "name": url[1],
+                "created_at": url[2],
+                "status_code": url[3]
+            })
+    finally:
+        release_conn(conn)
+
     return render_template("urls_list.html", urls=urls)
 
 
 @app.route("/urls/<int:url_id>/checks", methods=['POST'])
 def check_url(url_id):
-    conn = get_connection()
+    conn = get_conn()
     cursor = conn.cursor()
-    cursor.execute("SELECT name FROM urls WHERE id = %s", (url_id,))
-    url = cursor.fetchone()[0]
-
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.exceptions.RequestException:
-        flash('Произошла ошибка при проверке', 'danger')
-        return redirect(url_for('show_url', url_id=url_id))
+        cursor.execute("SELECT name FROM urls WHERE id = %s", (url_id,))
+        url = cursor.fetchone()[0]
 
-    soup = BeautifulSoup(response.content, 'lxml')
-    h1_tag = soup.find('h1')
-    h1_text = h1_tag.text if h1_tag else ""
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.exceptions.RequestException:
+            flash('Произошла ошибка при проверке', 'danger')
+            return redirect(url_for('show_url', url_id=url_id))
 
-    title_tag = soup.find('title')
-    title_text = title_tag.text if title_tag else ""
+        soup = BeautifulSoup(response.content, 'lxml')
+        h1_tag = soup.find('h1')
+        h1_text = h1_tag.text if h1_tag else ""
 
-    meta_description_tag = soup.find('meta', attrs={'name': 'description'})
-    description_text = meta_description_tag[
-        'content'] if meta_description_tag else ""
+        title_tag = soup.find('title')
+        title_text = title_tag.text if title_tag else ""
 
-    cursor.execute("""
-    INSERT INTO url_checks(
-        url_id, created_at, status_code, h1, description, title)
-    VALUES (%s, DATE(NOW()), %s, %s, %s, %s)
-    RETURNING id
-    """, (url_id, response.status_code, h1_text, description_text, title_text))
-    conn.commit()
-    flash('Страница успешно проверена', 'success')
+        meta_description_tag = soup.find('meta', attrs={'name': 'description'})
+        description_text = meta_description_tag[
+            'content'] if meta_description_tag else ""
+
+        cursor.execute("""
+        INSERT INTO url_checks(
+            url_id, created_at, status_code, h1, description, title)
+        VALUES (%s, DATE(NOW()), %s, %s, %s, %s)
+        RETURNING id
+        """, (url_id, response.status_code, h1_text, description_text, title_text))
+        conn.commit()
+        flash('Страница успешно проверена', 'success')
+    finally:
+        release_conn(conn)
+
     return redirect(url_for('show_url', url_id=url_id))
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,7 +1,15 @@
-import psycopg2
+from psycopg2.pool import SimpleConnectionPool
 import os
 
+db_pool = None
 
-def get_connection():
-    # Connect to your postgres DB
-    return psycopg2.connect(os.getenv("DATABASE_URL"))
+def init_db_pool():
+    global db_pool
+    db_url = os.getenv("DATABASE_URL")
+    db_pool = SimpleConnectionPool(1, 20, db_url)
+
+def get_conn():
+    return db_pool.getconn()
+
+def release_conn(conn):
+    db_pool.putconn(conn)
